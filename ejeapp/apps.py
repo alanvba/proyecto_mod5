b370aaf8ccfad850579408591cde659a2235bd7a
alan <alanvba@gmail.com>
2023-09-29 23:22:49 -0400

    inicio proyecto

---
+++ ejeapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EjeappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ejeapp'
---
+++ ejeapp/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+class tipo_hoja_ruta(models.Model):
+    descripcion = models.CharField(max_length=500,unique=False)
+    estado = models.BooleanField(blank=True, default=True)
+
+class hoja_ruta(models.Model):
+    numero = models.IntegerField()
+    remitente = models.CharField(max_length=500,unique=False)
+    referencia = models.CharField(max_length=500,unique=False)
+    prioritario = models.BooleanField(blank=True, default=False)
+    tipohoja_ruta = models.ForeignKey(tipo_hoja_ruta, on_delete=models.CASCADE)
+    fechahora = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class usuario(models.Model):
+    usuario = models.CharField(max_length=500,unique=False)
+    passwords = models.CharField(max_length=500,unique=False)
+    nombres = models.CharField(max_length=500,unique=False)
+    apellidos = models.CharField(max_length=500,unique=False)
+    estado = models.BooleanField(blank=True, default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class RemisionEstado(models.TextChoices):
+    ENVIADO = 'ev', 'Enviado'
+    PENDIENTE = 'pd', 'Pendiente'
+    RECIBIDO = 're', 'Recibido'
+
+class remision(models.Model):
+    hoja_ruta = models.ForeignKey(hoja_ruta, on_delete=models.CASCADE)
+    idusuario_origen = models.IntegerField()
+    idusuario_destino = models.IntegerField()
+    estado = models.CharField(
+        max_length=2,
+        choices=RemisionEstado.choices,
+        default=RemisionEstado.ENVIADO
+    )
+    habilitado = models.BooleanField(blank=True, default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class seguimiento_remision(models.Model):
+    remision = models.ForeignKey(remision, on_delete=models.CASCADE)
+    idusuario_origen = models.IntegerField()
+    idusuario_destino = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
